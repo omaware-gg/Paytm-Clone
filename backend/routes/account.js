7d55e35880f8914dbdb6de4ef26f81a410c3d522
omaware-gg <aware.om@kgpian.iitkgp.ac.in>
2024-09-01 17:35:20 +0530

    fixed a bug

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -42,7 +42,7 @@ router.post("/transfer", authMiddleware, async (req, res) => {
 
     //perform the transfer
     await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
-    await Account.updateOne({ userId: to }, { $inc { balance: amount } }).session(session);
+    await Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
 
     //commit the transaction
     await session.commitTransaction();
